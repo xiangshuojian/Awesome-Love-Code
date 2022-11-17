cb5d43270de2ab8b125600a396e06c224310219b
xiangshuojian <118423573+xiangshuojian@users.noreply.github.com>
2022-11-17 19:12:36 +0800

    Update index.js

--- Web/015-3D_Fireworks/js/index.js
+++ Web/015-3D_Fireworks/js/index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3||某某某|我们|在一起吧|❤|#rectangle|');
+      S.UI.simulate('|#countdown 3||向硕俭|王杰|❤|#rectangle|');
     }
 
     S.Drawing.loop(function () {
